9e4f5690e49c2015e89e937a617abbe7c03b6843
Gabriel Cohavy <gcohavy@gmail.com>
2021-01-19 12:44:13 -0800

    implemented ball and enemy basic movements

--- First Person Pong/Assets/Scripts/Controllers/BallController.cs
+++ First Person Pong/Assets/Scripts/Controllers/BallController.cs
@@ -5,16 +5,51 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     private Vector3 velocity = new Vector3();
+    private Rigidbody ballRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballRb = gameObject.GetComponent<Rigidbody>();
+        ResetBall();
+        BeginGame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ballRb.velocity = velocity;
+    }
+
+    Vector3 ReturnRandomVelocity()
+    {
+        return new Vector3(Random.Range(0,10.0f), 0, Random.Range(0, 10.0f));
+    }
+
+    public void ResetBall()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    public void BeginGame()
+    {
+        velocity = ReturnRandomVelocity();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.name)
+        {
+            case "PlayerPaddle": 
+            case "EnemyPaddle":
+                velocity.z = -velocity.z;
+                break;
+            case "BorderL":
+            case "BorderR":
+                velocity.x = -velocity.x;
+                break;
+            default:
+                break;
+        }
     }
 }
--- First Person Pong/Assets/Scripts/Controllers/EnemyController.cs
+++ First Person Pong/Assets/Scripts/Controllers/EnemyController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    GameObject gameBall;
+    Rigidbody ballRb;
+    public float speed = 10;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameBall = GameObject.Find("Ball");
+        ballRb = gameBall.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ballRb.velocity.z < 0){
+            if(gameBall.transform.position.x < transform.position.x)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+            }
+            else if(gameBall.transform.position.x > transform.position.x)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * speed);
+            }
+        }
     }
 }
