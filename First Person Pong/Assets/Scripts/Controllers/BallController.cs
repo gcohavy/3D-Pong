8bde31f75bf1f1ba320082787ddbef9bcbaabbb0
Gabriel Cohavy <gcohavy@gmail.com>
2021-01-19 11:49:21 -0800

    organized scripts into folders

---
+++ First Person Pong/Assets/Scripts/Controllers/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Vector3 velocity = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
