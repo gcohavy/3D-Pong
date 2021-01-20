ad5d63b5634c3105d0d4ea649afc9e74b008a8ad
Gabriel Cohavy <gcohavy@gmail.com>
2021-01-20 09:37:23 -0800

    created Singleton class

---
+++ First Person Pong/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T _instance;
+
+    public static T Instance 
+    {
+        get { return _instance;}
+    }
+
+    public static bool IsInitialized()
+    {
+        return _instance != null;
+    }
+
+    protected virtual void Awake()
+    {
+        if(Instance == null)
+        {
+            _instance = (T) this;
+        }
+        else
+        {
+            Debug.LogError("[Singleton] Cannot create multiple instances of " + _instance);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(_instance == this)
+        {
+            _instance = null;
+        }
+    }
+}
